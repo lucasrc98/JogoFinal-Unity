b715547339514233dc50154682b3f7d3c6b7a181
bruno.pinho <brunopinho@alu.ufc.br>
2019-06-28 01:56:35 -0300

    Pausa funcionando

---
+++ Assets/scripts/menuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+
+    
+    private void Update(){
+
+        if(Input.GetKey(KeyCode.Escape)){
+            isPaused = !isPaused;
+        }
+        if (isPaused){
+            AtivarMenu();
+        }
+        else {
+            DesativarMenu();
+        }
+    }
+
+    void AtivarMenu()
+    {
+        AudioListener.pause = true;
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+    public void DesativarMenu()
+    {
+        AudioListener.pause = false;
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void sairDoJogo()
+    {
+        Application.Quit();
+    }
+}
